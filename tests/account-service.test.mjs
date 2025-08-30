c0f80c7c1d6ed6446861a9cd96c2b38ca1cd0c44
Leonardo Carbornar <lcarbornar@gmail.com>
2025-08-29 23:26:09 -0300

    Test: Added unit test to the GetBalance and CreateAccount

---
+++ tests/account-service.test.mjs
@@ -0,0 +1,40 @@
+import AccountRepository from '../src/repository/account-repository.js' 
+import AccountService from '../src/services/account-service.js'
+
+let account_service
+
+beforeEach(() => {
+  const repository = new AccountRepository()
+  account_service = new AccountService(repository)
+})
+
+test('Should return 404 if account balance not found', () => {
+
+  const account_id = '999'
+
+  expect(() => account_service.GetAccountBalance(account_id)).toThrow('Account not found')
+
+})
+
+test('Should create a new account with initial balance', () => {
+
+  const account_id = '123'
+  const initial_balance = 100
+
+  account_service.CreateAccount(account_id, initial_balance)
+
+  expect(account_service.GetAccountBalance(account_id)).toBe(initial_balance)
+})
+
+test('Should return the balance', () => {
+
+  const account_id = '333'
+  const initial_balance = 42
+
+  account_service.CreateAccount(account_id, initial_balance)
+
+  const account_balance = account_service.GetAccountBalance(account_id)
+
+  expect(account_balance).toBeDefined()
+  expect(account_balance).toBe(42)
+})
