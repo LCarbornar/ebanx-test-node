956cf1ae6e3941d32d541c656c0f2308083b4f60
Leonardo Carbornar <lcarbornar@gmail.com>
2025-08-30 09:02:07 -0300

    Test: Created unit tests for transfer event

--- tests/account-service.test.mjs
+++ tests/account-service.test.mjs
@@ -63,3 +63,33 @@ test('Should return 404 if the account is not found when withdrawing funds', () 
   expect(() => account_service.Withdraw(account_id, withdraw_amount)).toThrow('Account not found')
 
 })
+
+test('Should transfer funds between accounts', () => {
+
+  const source_account_id = '555'
+  const destination_account_id = '666'
+  const initial_balance = 100
+  const transfer_amount = 50
+
+  account_service.Deposit(source_account_id, initial_balance)
+
+  account_service.Transfer(source_account_id, destination_account_id, transfer_amount)
+
+  const source_balance = account_service.GetAccountBalance(source_account_id)
+  const destination_balance = account_service.GetAccountBalance(destination_account_id)
+
+  expect(source_balance).toBeDefined()
+  expect(source_balance).toBe(50)
+  expect(destination_balance).toBeDefined()
+  expect(destination_balance).toBe(50)
+})
+
+test('Should return 404 if the account is not found when transferring funds', () => {
+
+  const source_account_id = '999'
+  const destination_account_id = '888'
+  const transfer_amount = 50
+
+  expect(() => account_service.Transfer(source_account_id, destination_account_id, transfer_amount)).toThrow('Account not found')
+
+})
